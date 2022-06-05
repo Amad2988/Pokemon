2bec7554ee5fde6d04146dec437692f51780dcf2
amad2988 <uddinwork@gmail.com>
2022-06-05 11:01:08 -0400

    new commit

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const { mongo, default: mongoose } = require('mongoose')
+const pocketmonsters = require('./Models/pocketmonsters')
 const app = express()
 const PORT = 3000
 const pokemon = require("./Models/pokemon")
+require('dotenv').config()
+console.log(process.env.MONGODB_URI)
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 
 app.set('view engine', 'ejs')
 app.set('views','./Views')
@@ -11,14 +21,60 @@ app.get('/', (req, res)=> {
     res.send('Welcome to the Pokemon App!')
 })
 
-app.get('/pokemon',(req, res) => {
-    res.render('Index', {data: pokemon})
+app.get('/pokemon', async (req, res) => {
+    try{
+    const pokemons = await pocketmonsters.find()
+
+    res.render('Index', {
+        pageTitle: 'Pokemon',
+        pageHeader: 'See All the Pokemon',
+        pokemon: pokemons,
+    })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.get('/pokemon/new', (req, res) => {
+    res.render('New', {
+        pageTitle: 'Create',
+        pageHeader: 'Create a new pokemon'
+    })
+});
+
+app.post('/pokemon', async (req, res) => {
+    const newPokemon = req.body
+    newPokemon.img = `http://img.pokemondb.net/artwork/${req.body.name}`
+    console.log(newPokemon)
+    await pocketmonsters.create(newPokemon, (err, result) =>{
+        if(err){
+            console.log(err)
+        }
+        console.log(result);
+    })
 })
 
-app.get('/pokemon/:id',(req, res) => {
-    res.send(req.params.id)
+app.get('/pokemon/:id', async (req, res) => {
+   try{
+    console.log(req.params.id)
+    const getPokemon = await pocketmonsters.findById(req.params.id)
+    console.log('Pokemon Found!', getPokemon)
+    
+
+    res.render('Show', {
+        pageTitle: 'PokeDex',
+        pageHeader: " Gotta Catch 'Em All ",
+        pokemon: getPokemon,
+    });
+} catch (error){
+    console.log(error)
+}
+
 })
+
 
 app.listen(PORT, () =>{
-    console.log(`Server is running on port: ${PORT}`)
+    console.log(`Server is running on port: ${PORT}`);
+    mongoose.connect(process.env.MONGODB_URI)
+    console.log('MongoDB is connected')
 })
